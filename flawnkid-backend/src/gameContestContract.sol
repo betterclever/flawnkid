7fe019e2fa8bba4d103b1f99b021a6b7783fed95
betterclever <paliwal.pranjal83@gmail.com>
2018-03-17 23:37:38 +0530

    added basic game contest smart contract

--- flawnkid-backend/src/gameContestContract.sol
+++ flawnkid-backend/src/gameContestContract.sol
@@ -1,46 +1,48 @@
 pragma solidity ^0.4.18;
 
-contract Voting {
-  /* mapping field below is equivalent to an associative array or hash.
-  The key of the mapping is candidate name stored as type bytes32 and value is
-  an unsigned integer to store the vote count
-  */
+
+contract GameContest {
+
+  struct StakeHolder {
+    address addr;
+    uint amount;
+  }
+
+  struct GameIdea {
+    string topic;
+    string description;
+    uint numStakeHolders;
+    uint amount;
+    mapping (uint => StakeHolder) stakeHolders;
+  }
   
-  mapping (bytes32 => uint8) public votesReceived;
+  uint numGameIdeas;
+  mapping (uint => GameIdea) gameIdeas;
+
+  function newGameIdea(string topic,
+                      string description
+                      ) returns (uint gameIdeaID) 
+  {
+    gameIdeaID = numGameIdeas++; 
+    gameIdeas[gameIdeaID] = GameIdea(topic, description, 0, 0);
+    GameIdea g = gameIdeas[gameIdeaID];
+    g.stakeHolders[g.numStakeHolders++] = StakeHolder({addr: msg.sender, amount: msg.value});
+    g.amount += msg.value;
+  }
   
-  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
-  We will use an array of bytes32 instead to store the list of candidates
-  */
-  
-  bytes32[] public candidateList;
-
-  /* This is the constructor which will be called once when you
-  deploy the contract to the blockchain. When we deploy the contract,
-  we will pass an array of candidates who will be contesting in the election
-  */
-  function Voting(bytes32[] candidateNames) public {
-    candidateList = candidateNames;
+  function addStake(uint gameIdeaID) {
+    GameIdea c = gameIdeas[gameIdeaID];
+    c.stakeHolders[c.numStakeHolders++] = StakeHolder({addr: msg.sender, amount: msg.value});
+    c.amount += msg.value;
   }
 
-  // This function returns the total votes a candidate has received so far
-  function totalVotesFor(bytes32 candidate) view public returns (uint8) {
-    require(validCandidate(candidate));
-    return votesReceived[candidate];
-  }
+  // function checkGoalReached(uint gameIdeaID) returns (bool reached) {
+  //   GameIdea c = gameIdeas[gameIdeaID];
+  //   if (c.amount < c.fundingGoal)
+  //     return false;
+  //   c.beneficiary.send(c.amount);
+  //   c.amount = 0;
+  //   return true;
+  // }
 
-  // This function increments the vote count for the specified candidate. This
-  // is equivalent to casting a vote
-  function voteForCandidate(bytes32 candidate) public {
-    require(validCandidate(candidate));
-    votesReceived[candidate] += 1;
-  }
-
-  function validCandidate(bytes32 candidate) view public returns (bool) {
-    for(uint i = 0; i < candidateList.length; i++) {
-      if (candidateList[i] == candidate) {
-        return true;
-      }
-    }
-    return false;
-  }
 }
