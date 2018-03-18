0f470879972df62e9161f6dafa49a2e72ec14f5d
betterclever <paliwal.pranjal83@gmail.com>
2018-03-18 07:06:10 +0530

    Made endpoints for api

--- flawnkid-backend/src/gameContestContract.sol
+++ flawnkid-backend/src/gameContestContract.sol
@@ -12,7 +12,6 @@ contract GameContest {
   struct Submission {
     address addr;
     string previewLink;
-    uint votes;
   }
 
   struct GameIdea {
@@ -26,40 +25,42 @@ contract GameContest {
   }
   
   uint numGameIdeas;
+
+  event newIdeaSubmitted(uint ideaID);
+
   mapping (uint => GameIdea) gameIdeas;
 
-  function newGameIdea(string topic,
-                      string description
-                      ) returns (uint gameIdeaID) 
-  {
-    gameIdeaID = numGameIdeas++; 
+  function newGameIdea(string topic, string description)  {
+    uint gameIdeaID = numGameIdeas++; 
+    emit newIdeaSubmitted(gameIdeaID);
     gameIdeas[gameIdeaID] = GameIdea(topic, description, 0, 0, 0);
-    GameIdea g = gameIdeas[gameIdeaID];
-    g.stakeHolders[g.numStakeHolders++] = StakeHolder({addr: msg.sender, amount: msg.value, claimed: false});
-    g.amount += msg.value;
   }
 
-  function addSubmission(uint gameIdeaID, string previewLink)
-    returns (uint submissionID) 
-  {
+  function addStake(uint gameIdeaID) payable {
+    GameIdea c = gameIdeas[gameIdeaID];
+    c.stakeHolders[c.numStakeHolders++] = StakeHolder({addr: msg.sender, amount: msg.value, claimed: false});
+    c.amount += msg.value;
+  }
+
+  function addSubmission(uint gameIdeaID, string previewLink) returns (uint submissionID) {
     GameIdea idea = gameIdeas[gameIdeaID];
     submissionID = idea.numSubmissions++;
     idea.submissions[submissionID] = Submission(
-       msg.sender, previewLink,  0
+       msg.sender, previewLink
     );
+    return submissionID;
   }
 
   function buySubmission(uint gameIdeaID, uint submissionID) returns(bool status) {
     GameIdea idea = gameIdeas[gameIdeaID];
     Submission s = idea.submissions[submissionID];
-    
+
     for (uint index = 0; index < idea.numStakeHolders; index++) {
       StakeHolder st = idea.stakeHolders[index];
       if(st.addr == msg.sender) {
         if(!st.claimed) {
           s.addr.send(st.amount);
           idea.amount -= st.amount;
-          idea.numStakeHolders -= 1;
           st.claimed = true;
           return true;
         } else {
@@ -73,17 +74,12 @@ contract GameContest {
     return false;
   }
   
-  function addStake(uint gameIdeaID) {
-    GameIdea c = gameIdeas[gameIdeaID];
-    c.stakeHolders[c.numStakeHolders++] = StakeHolder({addr: msg.sender, amount: msg.value, claimed: false});
-    c.amount += msg.value;
-  }
 
   // function checkGoalReached(uint gameIdeaID) returns (bool reached) {
   //   GameIdea c = gameIdeas[gameIdeaID];
   //   if (c.amount < c.fundingGoal)
   //     return false;
-  //   c.beneficiary.send(c.amount);
+  //   c.beneficiary..send(c.amount);
   //   c.amount = 0;
   //   return true;
   // }
